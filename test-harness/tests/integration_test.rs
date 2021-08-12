63e0fd7452b12435ca3ac2cf403fb6ef88fa3c6d
Teddy Heinen <teddy@heinen.dev>
2021-08-12 11:51:41 -0500

    supress stdout/err when running test-harness

--- test-harness/tests/integration_test.rs
+++ test-harness/tests/integration_test.rs
@@ -1,23 +1,35 @@
+fn run_test_harness(bin: impl AsRef<str>) -> Result<Option<i32>, std::io::Error> {
+    Ok(std::process::Command::new("cargo")
+        .arg("run")
+        .arg("--bin")
+        .arg(bin.as_ref())
+        .stdout(std::process::Stdio::null())
+        .stderr(std::process::Stdio::null())
+        .spawn()?
+        .wait()?
+        .code())
+}
+
 #[test]
-fn ctf() -> Result<(), std::io::Error>{
-    assert_eq!(std::process::Command::new("cargo").arg("run").arg("--bin").arg("ctf").spawn()?.wait()?.code(), Some(0));
+fn ctf() -> Result<(), std::io::Error> {
+    assert_eq!(run_test_harness("ctf")?, Some(0));
     Ok(())
 }
 
 #[test]
-fn no_subcommands() -> Result<(), std::io::Error>{
-    assert_eq!(std::process::Command::new("cargo").arg("run").arg("--bin").arg("no_subcommands").spawn()?.wait()?.code(), Some(0));
+fn no_subcommands() -> Result<(), std::io::Error> {
+    assert_eq!(run_test_harness("no_subcommands")?, Some(0));
     Ok(())
 }
 
 #[test]
-fn multiple_schema() -> Result<(), std::io::Error>{
-    assert_eq!(std::process::Command::new("cargo").arg("run").arg("--bin").arg("multiple_schema").spawn()?.wait()?.code(), Some(0));
+fn multiple_schema() -> Result<(), std::io::Error> {
+    assert_eq!(run_test_harness("multiple_schema")?, Some(0));
     Ok(())
 }
 
 #[test]
-fn builder() -> Result<(), std::io::Error>{
-    assert_eq!(std::process::Command::new("cargo").arg("run").arg("--bin").arg("builder").spawn()?.wait()?.code(), Some(0));
+fn builder() -> Result<(), std::io::Error> {
+    assert_eq!(run_test_harness("builder")?, Some(0));
     Ok(())
 }
